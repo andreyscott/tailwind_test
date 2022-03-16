4344a867af807e210e22d6a565c042ee6bc0aafb
andreyscott <andreyscott>
2022-03-16 09:48:34 +0100

    tested out a header component

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# tailwind_test
+A project created using tailwind components found online for testing purposes before bring transfer to a my main project
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import DropDownMenu from './components/menu';
+
+function App() {
+  return (
+    <div className="App">
+     <DropDownMenu />
+    </div>
+  );
+}
+
+export default App;
